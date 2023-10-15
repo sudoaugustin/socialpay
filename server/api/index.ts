c57109afee922d739d90c79f096076b39f10c17d
sudoaugustin <sudoaugustin@gmail.com>
2023-10-15 15:02:53 +0630

    add: / handler to check if server work on vercel

--- server/api/index.ts
+++ server/api/index.ts
@@ -17,6 +17,10 @@ mongoose
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+  res.json('🤟 🤟 🤟').end();
+});
+
 app.use('/auth', auth);
 app.use('/bank', bank);
 app.use('/user', user);
